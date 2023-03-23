8c7f89d2b0045d74916cc79031eb988c8eed3ff5
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-03-23 10:55:05 +0100

    Fix, Bot can now go Online

--- bot.js
+++ bot.js
@@ -1,19 +1,16 @@
-const token = 'MTA4ODAxMzI3OTI1NjkxNjAMTA4ODAxMzI3OTI1NjkxNjA0OA.Gn7Jqg.topN23Z18g9EDG3ZnjQdI02TxMOnclOtFAilWw0OA.Gn7Jqg.topN23Z18g9EDG3ZnjQdI02TxMOnclOtFAilWw'
+const token = 'MTA4ODAxMzI3OTI1NjkxNjA0OA.GpH65Y.TJBTn30j4jzEdCuvtIxG48739BZYbG2L8qbjFY'
 
 console.log('tickle');
-const { Client, Events, GatewayIntentBits } = require('discord.js'); const client = new Client({
-intents: [
-GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages,
-],
-    partials: ['MESSAGE', 'CHANNEL']
-});
+const { Client, Events, GatewayIntentBits } = require('discord.js'); 
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages,],partials: ['MESSAGE', 'CHANNEL']});
+
 client.login(token);
 
 client.once(Events.ClientReady, onReady);
 
 function onReady() {
-console.log(`Ready! Logged in as ${c.user.tag}`);
-};
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+    };
 
 client.on(Events.MessageCreate, onMessage);
 
