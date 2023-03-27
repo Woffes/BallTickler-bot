5d9ba3bfa6809de22714acc56179d33c5a33f309
Woffes <squidg4ce@gmail.com>
2023-03-27 14:58:08 +0200

    update 27/3-23

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const token = 'MTA4ODAxMzI3OTI1NjkxNjA0OA.GZh5Vk.8_oBD2A4X1wNFv0OwQaOlXnEphaKWyTk59x6ls'
 
 console.log('tickle');
@@ -12,11 +14,19 @@ function onReady() {
     console.log(`Ready! Logged in as ${client.user.tag}`);
     };
 
+client.login(process.env.tokenName);
+
 client.on(Events.MessageCreate, onMessage);
 
 function onMessage(msg){
-    if (msg.content.includes('hej')){
-        msg.reply('tickle');
+    if(message.author.bot) {
+      return;
     }
-    console.log(msg);
-}
+
+    if(msg.content.includes('!ask')){
+      const answers = ["tickle","fondle","stomp","squish","lick"]
+      let answer = answers[Math.floor(Math.random() * answers.length)]
+      msg.reply(answer)
+      console.log(answer + " : " + Math.floor(Math.random() * answers.length))
+    }
+   }   
