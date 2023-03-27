48bd4a943dc065ba63fcc81892db304fc1658133
Woffes <squidg4ce@gmail.com>
2023-03-27 16:26:00 +0200

    new up

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 
-const token = 'MTA4ODAxMzI3OTI1NjkxNjA0OA.GZh5Vk.8_oBD2A4X1wNFv0OwQaOlXnEphaKWyTk59x6ls'
-
 console.log('tickle');
 const { Client, Events, GatewayIntentBits } = require('discord.js'); 
+
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages,],partials: ['MESSAGE', 'CHANNEL']});
 
-client.login(token);
+client.login(process.env.BOT_TOKEN);
 
 client.once(Events.ClientReady, onReady);
 
@@ -14,19 +13,20 @@ function onReady() {
     console.log(`Ready! Logged in as ${client.user.tag}`);
     };
 
-client.login(process.env.tokenName);
-
 client.on(Events.MessageCreate, onMessage);
 
-function onMessage(msg){
+function onMessage(message){
     if(message.author.bot) {
       return;
     }
 
-    if(msg.content.includes('!ask')){
+    command = message.content.substring(message.content.indexOf("!"));
+  console.log(command)
+    if(command.startsWith('!ask')){
       const answers = ["tickle","fondle","stomp","squish","lick"]
+
       let answer = answers[Math.floor(Math.random() * answers.length)]
-      msg.reply(answer)
-      console.log(answer + " : " + Math.floor(Math.random() * answers.length))
+
+    message.reply(answer)
     }
    }   
